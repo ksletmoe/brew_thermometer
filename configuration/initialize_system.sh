5e28631fecda87e8bbdfdb0be0cffcc9d221d124
Kyle Sletmoe <kyle.sletmoe@gmail.com>
2016-04-23 16:28:06 -0700

    updated initialize_system.sh to set up the log directory and permissions

--- configuration/initialize_system.sh
+++ configuration/initialize_system.sh
@@ -4,10 +4,19 @@ PI_USER="pi"
 CONF_DIR="/etc/brew_thermometer"
 DEFAULT_CONF_FILE="default_configuration.json"
 CONF_FILE_NAME="config.json"
+LOG_DIR="/var/log/brew_thermometer/"
 
+echo "configuring brew_thermometer configuration directory..."
 mkdir -p "$CONF_DIR"
 chown -R "$PI_USER":"$PI_USER" "$CONF_DIR"
 chmod -R 700 "$CONF_DIR"
 cp "$DEFAULT_CONF_FILE" "$CONF_DIR/$CONF_FILE_NAME"
+
+echo "configuring brew_thermometer log directory..."
+mkdir -P "$LOG_DIR"
+chown -R "$PI_USER":"$PI_USER" "$LOG_DIR"
+chmod -R 700 "$LOG_DIR"
+
+
 
 
